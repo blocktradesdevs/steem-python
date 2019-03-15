5c9badcd0d953c202389acf88f4407dee64ebbb4
Dariusz Kedzierski <dkedzierski@syncad.com>
2019-03-15 12:31:26 +0100

    Debug api tests, setup udated

--- tests/steem/test_debug.py
+++ tests/steem/test_debug.py
@@ -3,6 +3,7 @@ from uuid import uuid4
 from time import sleep
 import pytest
 import os
+import json
 
 TEST_NODES = [os.environ.get("STEEM_TEST_NODE", None)]
 WIF = os.environ.get("STEEM_TEST_WIF", None)
@@ -15,7 +16,7 @@ def test_evn_variables():
 def test_debug_has_hardfork():
     s = Steem(nodes = TEST_NODES, no_broadcast = False, keys = [WIF])
     ret = s.debug_has_hardfork(0)
-    print(ret)
+    assert ret['has_hardfork'] == True, "Ret: {}".format(ret)
 
 def test_debug_get_witness_schedule():
     s = Steem(nodes = TEST_NODES, no_broadcast = False, keys = [WIF])
@@ -26,19 +27,19 @@ def test_debug_get_witness_schedule():
 def test_debug_get_hardfork_property_object():
     s = Steem(nodes = TEST_NODES, no_broadcast = False, keys = [WIF])
     ret = s.debug_get_hardfork_property_object()
-    print(ret)
+    assert ret['last_hardfork'] == 0, "Ret: {}".format(ret)
 
 
 def test_debug_get_json_schema():
     s = Steem(nodes = TEST_NODES, no_broadcast = False, keys = [WIF])
     ret = s.debug_get_json_schema()
-    print(ret)
+    assert ret['schema'] == '', "Ret: {}".format(ret)
 
 
 def test_debug_generate_blocks():
     s = Steem(nodes = TEST_NODES, no_broadcast = False, keys = [WIF])
     ret = s.debug_generate_blocks(WIF, 10, 0, 0, True)
-    print(ret)
+    assert ret['blocks'] == 10, "Ret: {}".format(ret)
 
 
 def test_debug_generate_blocks_until():
@@ -53,13 +54,20 @@ def test_debug_generate_blocks_until():
 def test_debug_set_hardfork():
     s = Steem(nodes = TEST_NODES, no_broadcast = False, keys = [WIF])
     ret = s.debug_set_hardfork(22)
-    print(ret)
+    assert len(ret) == 0, "Ret: {}".format(ret)
+
+    ret = s.debug_generate_blocks(WIF, 5, 0, 0, True)
+    assert ret['blocks'] == 5, "Ret: {}".format(ret)
+
+    ret = s.debug_has_hardfork(22)
+    assert ret['has_hardfork'] == True, "Ret: {}".format(ret)
 
 
 def test_debug_pop_block():
     s = Steem(nodes = TEST_NODES, no_broadcast = False, keys = [WIF])
     ret = s.debug_pop_block()
-    print(ret)
+    ret = ret.get("block", None)
+    assert ret is not None
 
 
 def test_debug_push_blocks():
@@ -71,6 +79,11 @@ def test_debug_push_blocks():
 if __name__ == "__main__":
     test_evn_variables()
     test_debug_has_hardfork()
+    test_debug_get_hardfork_property_object()
     test_debug_get_witness_schedule()
     test_debug_get_json_schema()
     test_debug_generate_blocks()
+    test_debug_set_hardfork()
+    test_debug_generate_blocks()
+    test_debug_pop_block()
+    test_debug_generate_blocks_until()
